6fe25f5e8cb7a562890183af09ca5a35a3057026
Zach Egner <egner.zach@gmail.com>
2023-04-14 12:35:10 -0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Heading, Image, Text } from '@chakra-ui/react'
+import logo from '../assets/light-bulb.svg'
+
+const Header = () => {
+  return (
+    <>
+      <Image src={logo} alt='logo' width={100} marginBottom='1rem' />
+      <Heading color='white' marginBottom='1rem'>
+        AI Keyword Extracter
+      </Heading>
+      <Text>
+        Paste in your text below
+      </Text>
+    </>
+  )
+}
+
+export default Header
